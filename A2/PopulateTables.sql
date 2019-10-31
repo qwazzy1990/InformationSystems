e4a4f8bc6f9a54aeb813639233176f8ce373c82a
qwazzy1990 <disalvopatrick@gmail.com>
2019-10-30 22:55:19 -0400

    ss

--- A2/PopulateTables.sql
+++ A2/PopulateTables.sql
@@ -1,1 +1,15 @@
-INSERT INTO Topics (topic_number, topic) VALUES (301, 'Machine Learning');
+INSERT INTO Topics VALUES 
+    (301, 'Machine Learning'),
+    (302, 'Databases'),
+    (303, 'Big Data'),
+    (304, 'Programming Languages');
+
+INSERT INTO Projects Values 
+    (1001, 'a unified database for structured/unstructured/semistructured data', 302),
+    (1001, 'a unified database for structured/unstructured/semistructured data', 303),
+    (1002, 'advanced machine learning in analyzing crop images', 301),
+    (1003, 'efficient functional programming in bib data', 303),
+    (1003, 'efficient functional programming in bib data', 304);
+
+
+
