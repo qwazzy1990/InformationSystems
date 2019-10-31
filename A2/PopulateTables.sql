0776423870550db3d229ea81a03bd4cdc2376da7
qwazzy1990 <disalvopatrick@gmail.com>
2019-10-30 22:47:04 -0400

    f

--- A2/PopulateTables.sql
+++ A2/PopulateTables.sql
@@ -1,4 +1,1 @@
-INSERT INTO Topics (topic_number, topic) VALUES (301, "Machine Learning"),
-    -- (302, "Databases"),
-    -- (303, "Big Data"),
-    -- (304, "Programming Languages"); 
+INSERT INTO Topics (topic_number, topic) VALUES (301, "Machine Learning");
