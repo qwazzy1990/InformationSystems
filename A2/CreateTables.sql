d265678c6dd29740510f90cbc32d7cccf0732f9b
qwazzy1990 <disalvopatrick@gmail.com>
2019-10-30 20:51:11 -0400

    created create and drop tables

--- A2/CreateTables.sql
+++ A2/CreateTables.sql
@@ -1,1 +1,6 @@
-CREATE TABLE TestTable(ID integer, name char);
+CREATE TABLE Projects(id integer NOT NULL, project_name varchar(50) NOT NULL, pi boolean NOT NULL, grant_id integer, topic_id integer, researcher_id);
+CREATE TABLE Topics(id integer NOT NULL, topic_name varchar(50) NOT NULL, project_id integer, report_id integer);
+CREATE TABLE Reports(id integer NOT NULL, title varchar(50), researcher_id integer);
+CREATE TABLE FundingAgencies(id varchar(50) NOT NULL, address varchar(300), grant_id integer);
+CREATE TABLE Grants(id integer NOT NULL, amount integer, period varchar(50), project_id integer, agency_id varchar(50));
+CREATE TABLE Researchers(id integer, first_name varchar(50), last_name varchar(50), office varchar(50), is_pi boolean, email varchar(50), project_id integer, report_id integer);
---
+++ A2/DropTables.sql
@@ -0,0 +1,6 @@
+DROP Projects;
+DROP Topics;
+DROP Reports;
+DROP FundingAgencies;
+DROP Grants;
+DROP Researchers;
