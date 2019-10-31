a6a01178ea04949029a3ea97affd5e2a7f76f43f
qwazzy1990 <disalvopatrick@gmail.com>
2019-10-31 16:07:51 -0400

    FuncOne

---
+++ A2/FuncOne.sql
@@ -0,0 +1,5 @@
+CREATE FUNCTION (val integer) RETURNS void AS $$
+BEGIN
+    SELECT * FROM Researchers INNER JOIN Reports on researcher_number = val AND report_id = report_number;
+END; $$
+LANGUAGE PLPGSQL;
