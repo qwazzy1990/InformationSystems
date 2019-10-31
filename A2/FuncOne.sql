1f71a3e6d4ed9b858c92759869cc9db7a17dd3a7
qwazzy1990 <disalvopatrick@gmail.com>
2019-10-31 16:10:03 -0400

    Func On

--- A2/FuncOne.sql
+++ A2/FuncOne.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION (val integer) RETURNS void AS $$
+CREATE FUNCTION funcOne(val integer) RETURNS void AS $$
 BEGIN
     SELECT * FROM Researchers INNER JOIN Reports on researcher_number = val AND report_id = report_number;
 END; $$
