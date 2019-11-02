6994e49f02b1c3a451ffbb14b047bf6cc1fc777c
qwazzy1990 <disalvopatrick@gmail.com>
2019-11-02 02:27:55 -0400

    ss

--- A2/FuncTwo.sql
+++ A2/FuncTwo.sql
@@ -11,7 +11,9 @@ BEGIN
         fetch c1 into id, rname, isPi;
         exit when not found;
         if (isPi = true) then
+            RAISE NOTICE 'ID: %', id;
             RAISE NOTICE 'Name: %', rname;
+            RAISE NOTICE 'I do not like to share my income';
         end if;
     end loop;
     close c1;
