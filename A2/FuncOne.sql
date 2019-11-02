11c23c1cef48af9ab5d6bc889f5fcc2fb9dd9cef
qwazzy1990 <disalvopatrick@gmail.com>
2019-11-02 01:56:51 -0400

    ff

--- A2/FuncOne.sql
+++ A2/FuncOne.sql
@@ -1,13 +1,8 @@
 CREATE OR REPLACE FUNCTION queryOne(val integer) RETURNS TABLE(rn int, 
-    rname varchar(300), 
-    phone varchar(30), 
-    room varchar(30), 
-    email varchar(30),
-    prid int,
-    reportid int,
-    pi boolean) AS $table1$
+    title varchar(300),
+    topic int) AS $table1$
 BEGIN
     RETURN QUERY 
-    SELECT * FROM Researchers INNER JOIN Reports on researcher_number = val AND report_id = report_number;
+    SELECT report_number, title, topic_id FROM Researchers INNER JOIN Reports on researcher_number = val AND report_id = report_number;
 END; $table1$
 LANGUAGE PLPGSQL;
