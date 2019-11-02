a706e7661b69a88cc69b711c0c09131b43286b68
qwazzy1990 <disalvopatrick@gmail.com>
2019-11-02 01:37:41 -0400

    s

--- A2/FuncOne.sql
+++ A2/FuncOne.sql
@@ -1,5 +1,6 @@
 CREATE FUNCTION queryOne(val integer) RETURNS TABLE(num1 int, v2 varchar(300), num2 int) AS $$
 BEGIN
-    return SELECT * FROM Researchers INNER JOIN Reports on researcher_number = val AND report_id = report_number;
+    RETURN QUERY 
+    SELECT * FROM Researchers INNER JOIN Reports on researcher_number = val AND report_id = report_number;
 END; $$
 LANGUAGE PLPGSQL;
