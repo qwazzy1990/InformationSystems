91f5e7a86a295422802c23c01b66511ea6eca355
qwazzy1990 <disalvopatrick@gmail.com>
2019-11-02 01:33:29 -0400

    ff

--- A2/FuncOne.sql
+++ A2/FuncOne.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION funcOne(val integer) RETURNS TABLE(num1 int, v2 varchar(300), num2 int) AS $$
+CREATE FUNCTION queryOne(val integer) RETURNS TABLE(num1 int, v2 varchar(300), num2 int) AS $$
 BEGIN
     return SELECT * FROM Researchers INNER JOIN Reports on researcher_number = val AND report_id = report_number;
 END; $$
